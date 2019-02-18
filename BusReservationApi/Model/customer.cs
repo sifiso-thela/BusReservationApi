5341ca71c5c095eb0115481a6f453ad7f6aed343
queenzikalala <queen.zikalala>
2019-02-18 15:40:59 +0200

    ticket model

--- BusReservationApi/Model/customer.cs
+++ BusReservationApi/Model/customer.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace BusReservationApi.Model
 {
-    public class customer
+    public class Customer
     {
         public int Id { get; set; }
         public string Name { get; set; }
