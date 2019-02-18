9a5a218e5e91c81701fcec555369c18b9db43f7f
sifiso-thela <44636784+sifiso-thela@users.noreply.github.com>
2019-02-18 19:05:03 +0200

    added customerService

---
+++ BusReservationApi/Service/CustomerInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusReservationApi.Model;
+
+namespace BusReservationApi.Service
+{
+    interface CustomerInterface
+    {
+        IEnumerable<Customer> getAllCustomers();
+        Customer getCustomer(string id);
+        bool doesCustomerExist(string ig);
+        void createCustomer(Customer customer);
+        void updateCustomer(Customer customer);
+        void deleteCustomer(string id);
+        
+    }
+}
--- BusReservationApi/Service/CustomerService.cs
+++ BusReservationApi/Service/CustomerService.cs
@@ -2,11 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusReservationApi.Model;
 
 namespace BusReservationApi.Service
 {
-    public class CustomerService
+    public class CustomerService : CustomerInterface
     {
+        private List<Customer> customers;
         
+        public IEnumerable<Customer> getAllCustomers()
+        {
+            return this.customers;
+        }
+
+        public Customer getCustomer(string id)
+        {
+            return this.customers.FirstOrDefault(cust => cust.Id == id);
+        }
+
+        public bool doesCustomerExist(string id)
+        {
+            return this.customers.Any(cust => cust.Id == id);
+        }
+
+        public void createCustomer(Customer customer)
+        {
+            try
+            {
+                this.customers.Add(customer);
+            }catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+            
+        }
+
+        public void updateCustomer(Customer customer)
+        {
+            try
+            {
+                var cust = this.getCustomer(customer.Id);
+                var index = this.customers.IndexOf(cust);
+
+                this.customers.RemoveAt(index);
+                this.customers.Insert(index, customer);
+            }catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+        public void deleteCustomer(string id)
+        {
+            try
+            {
+                var cust = this.getCustomer(id);
+                var index = this.customers.IndexOf(cust);
+
+                this.customers.RemoveAt(index);
+            }catch(Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
     }
 }
