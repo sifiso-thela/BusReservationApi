1eeb8341d9f2335600f26856bd76e29b4672788f
queenzikalala <queen.zikalala>
2019-02-18 15:32:34 +0200

    customer model

---
+++ BusReservationApi/Model/customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusReservationApi.Model
+{
+    public class customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Next_of_kin { get; set; }
+    }
+}
