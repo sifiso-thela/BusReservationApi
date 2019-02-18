f94d8d41ee7ef72c948e061911122a6bce7d2e4c
queenzikalala <queen.zikalala>
2019-02-18 15:48:25 +0200

    agent model

---
+++ BusReservationApi/Model/Agent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusReservationApi.Model
+{
+    public class Agent 
+    {
+        public string id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
