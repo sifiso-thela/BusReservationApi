be22bc31c34772b6618a272e7ef3ae379aff7c9d
Maiki Molahlehi <molahlehimaiki@gmail.com>
2019-02-19 13:23:12 +0200

    added all data access classes

---
+++ BusReservationApi/DB/BusDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusReservationApi.Model;
+using MySql.Data;
+using MySql.Data.MySqlClient; 
+namespace BusReservationApi.DB
+{
+    public class BusDAO
+    {
+    }
+}
