6c5051b1163e6ae45c4dde0546b02466fd78b0e3
sifiso-thela <44636784+sifiso-thela@users.noreply.github.com>
2019-02-19 13:48:10 +0200

    delete stuff

--- BusReservationApi/Service/CustomerService.cs
+++ BusReservationApi/Service/CustomerService.cs
@@ -10,47 +10,20 @@ namespace BusReservationApi.Service
 {
     public class CustomerService
     {
-<<<<<<< HEAD
-        private List<Customer> customers;
-        /*
-        private CustomerContext _context;
-        
-        public CustomerService(CustomerContext context)
-        {
-            this._context = context;
-            
-        }
-        */
+        CustomerDAO customerDAO = new CustomerDAO();
+
         public CustomerService()
         {
-            customers = new List<Customer>()
-            {
-                new Customer("1", "Name", "Surname", "0659024156", "Next", "rt"),
-                new Customer("2", "sam", "khoza", "01234545678", "of", "yty"),
-                new Customer("3", "nkosi", "smith", "0974521575", "kin", "u")
-            };
         }
-        public IEnumerable<Customer> getAllCustomers()
-        {
-            return customers;
-=======
-        CustomerDAO customerDAO = new CustomerDAO();
 
         public IEnumerable<Customer> getAllCustomers()
         {
             return customerDAO.Get();
->>>>>>> 49c1e2150998980d6cfa62c130f61305f5c5f84b
         }
 
         public Customer getCustomer(int id)
         {
-<<<<<<< HEAD
-              
-           return customers.Where(cust => cust.Id == id).FirstOrDefault();
-
-=======
             return customerDAO.Get(id);
->>>>>>> 49c1e2150998980d6cfa62c130f61305f5c5f84b
         }
         /*
         public bool doesCustomerExist(string id)
