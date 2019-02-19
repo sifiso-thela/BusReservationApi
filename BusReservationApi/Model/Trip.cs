6dda85929cdc31a6204f534a65604c69e11c382a
queenzikalala <queen.zikalala>
2019-02-19 09:01:41 +0200

    trip model

---
+++ BusReservationApi/Model/Trip.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusReservationApi.Model
+{
+    public class Trip
+    {
+        public string id { get; set; }
+        public Bus  Bus { get; set; }
+        public DateTime Time { get; set; }
+        public DateTime Depature_time { get; set; }
+        public DateTime Arrival_time { get; set; }
+        public string Starting_point { get; set; }
+        public string Destination { get; set; }
+        public Driver Driver { get; set; }
+        public float Price { get; set; }
+    }
+}
