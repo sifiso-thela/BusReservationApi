86592937a599b435823fe175523c6b549bb12b6e
sifiso-thela <44636784+sifiso-thela@users.noreply.github.com>
2019-02-19 14:26:39 +0200

    Created AgentService and BusService

---
+++ BusReservationApi/Service/BusService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BusReservationApi.DB;
+using BusReservationApi.Model;
+namespace BusReservationApi.Service
+{
+    public class BusService
+    {
+        BusDAO busDAO = new BusDAO();
+
+        public BusService()
+        {
+        }
+        //ready for busDAO implementation
+        /*
+        public IEnumerable<Bus> GetAllBusses()
+        {
+            return busDAO.Get();
+        }
+
+        public Bus GetBus(int id)
+        {
+            return busDAO.Get(id);
+        }
+        
+        public Bus CreateCustomer(Bus bus)
+        {
+            return busDAO.Create(bus);
+        }
+
+        public Customer UpdateCustomer(Bus bus)
+        {
+            return busDAO.Update(bus);
+        }
+
+        public bool DeleteCustomer(int id)
+        {
+            return busDAO.Delete(id);
+        }
+        */
+    }
+}
