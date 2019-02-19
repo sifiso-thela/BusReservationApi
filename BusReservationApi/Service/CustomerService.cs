c3595973ba3c3101450fded8b5294fa73e6a395d
xolanisiqhelo <siqhelo25@gmail.com>
2019-02-19 14:01:31 +0200

    conflicts

--- BusReservationApi/Service/CustomerService.cs
+++ BusReservationApi/Service/CustomerService.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BusReservationApi.Model;
-using BusReservationApi.Controllers;
+//using BusReservationApi.Controllers;
 using BusReservationApi.DB;
 
 namespace BusReservationApi.Service
