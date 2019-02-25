ec5f9dcd7e57c529747a81bbc4d03e8f3cc71352
xolanisiqhelo <siqhelo25@gmail.com>
2019-02-25 08:55:57 +0200

    driver srvice

---
+++ BusReservationApi/Controllers/DriverController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BusReservationApi.DB;
+using BusReservationApi.Model;
+using BusReservationApi.Service;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BusReservationApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class DriverController : Controller
+    {
+        DriverService driverService = new DriverService();
+
+        // GET: api/<controller>
+        [HttpGet("all")]
+        public IEnumerable<Driver> GetAllDrivers()
+        {
+            return driverService.GetAllDrivers();
+        }
+
+        [HttpGet("{id}")]
+        public Driver GetDriver(int id)
+        {
+            return driverService.GetDriver(id);
+        }
+        
+
+
+        // POST api/<controller>
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- BusReservationApi/Service/DriverService.Service.cs
+++ BusReservationApi/Service/DriverService.Service.cs
@@ -10,7 +10,34 @@ namespace BusReservationApi.Service
 {
     public class DriverService
     {
-        
+        DriverDAO driverDAO = new DriverDAO();
+
+         public  IEnumerable<Driver> GetAllDrivers()
+        {
+            return driverDAO.Get();
+        }
+        public Driver GetDriver(int id)
+        {
+            return driverDAO.Get(id);
+        }
+
+        public Driver createDriver( Driver driver)
+        {
+            return driverDAO.CreateCustomer(driver);
+        }
+
+        public Driver  updateDriver(Driver driver)
+        {
+            return driverDAO.Update(driver);
+        }
+
+        public bool deleteDriver(int driver)
+        {
+            return driverDAO.Delete(driver);
+
+        }
+
+
 
 
 
